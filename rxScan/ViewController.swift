8b71bb6fa207f050f27118dac21ba4f1e93c3b26
Mac Air <macair@macs-macbook-air.local>
2016-07-31 18:44:16 +0700

    Initial Commit

---
+++ rxScan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rxScan
+//
+//  Created by Yodfha on 7/31/2559 BE.
+//  Copyright © 2559 Fhasai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
